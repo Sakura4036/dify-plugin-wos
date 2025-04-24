930ce5d9124ae7610c5d69190c2eceee8e89521f
Hjw <1973140351@qq.com>
2025-04-24 15:29:03 +0800

    fix validate_credentials error

--- provider/wos.py
+++ provider/wos.py
@@ -3,23 +3,14 @@ from typing import Any
 from dify_plugin import ToolProvider
 from dify_plugin.errors.tool import ToolProviderCredentialValidationError
 
-from tools.wos_search import WOSSearchTool
+from tools.wos_search import WosSearchAPI
 
 
 class WosProvider(ToolProvider):
     def _validate_credentials(self, credentials: dict[str, Any]) -> None:
         try:
-            WOSSearchTool().fork_tool_runtime(
-                runtime={
-                    "credentials": credentials,
-                }
-            ).invoke(
-                tool_parameters={
-                    "query": "test",
-                    "query_type": "TS",
-                    "limit": 10,
-                    "page": 1
-                },
+            WosSearchAPI(api_key=credentials.get('wos_api_key')).query_once(
+                query="test",
             )
         except Exception as e:
             raise ToolProviderCredentialValidationError(str(e))
